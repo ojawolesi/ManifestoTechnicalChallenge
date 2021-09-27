2d25f6270b986ea04a45a809049662bf9d5c2b15
psyoa3 <psyoa3@exmail.nottingham.ac.uk>
2021-09-27 08:22:43 +0100

    Manifesto Technical Challenge: completed

---
+++ src/test/java/CustomerAccountTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerAccountTest {
+
+    @Test
+    void checkMoney() {
+    }
+
+    @Test
+    void cashWithdrawal() {
+    }
+}
---
+++ src/test/java/SessionsTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionsTest {
+
+    @Test
+    void checkPin() {
+    }
+
+    @Test
+    void processPin() {
+    }
+
+    @Test
+    void endSession() {
+    }
+}
